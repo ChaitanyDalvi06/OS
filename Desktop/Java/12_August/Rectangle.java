d38d0a5230cef74555bc9e426f7c29c089390d79
Chaitanya Dalvi <142775927+ChaitanyDalvi06@users.noreply.github.com>
2025-09-28 12:37:42 +0530

    Delete Desktop/Java directory

--- Desktop/Java/12_August/Rectangle.java
+++
@@ -1,5 +0,0 @@
-class rectangle
-{
-    
-
-}
--- Desktop/Java/assignment2.2/ExceptionThrows.java
+++
@@ -1,8 +0,0 @@
-// Q7
-// WAP to demonstrate application of throws in exception handling.
-
-public class ExceptionThrows {
-    public static void main(String[] args) throws Exception {
-        throw new Exception("Exception thrown");
-    }
-}
